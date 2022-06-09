28cdb62e391298e8e07bfac013be8468068769c1
wangrui <wangruiself@163.com>
2022-06-09 12:51:45 +0800

    fix: fixed
    Require statement not part of import statement

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-var-requires': 0
   }
 }
