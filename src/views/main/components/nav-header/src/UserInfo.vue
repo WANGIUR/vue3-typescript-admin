ab89f7cfabc318dcc6552f791d6592a917094b22
wangrui <wangruiself@163.com>
2022-06-16 18:56:09 +0800

    feat: 退出登录

--- src/views/main/components/nav-header/src/UserInfo.vue
+++ src/views/main/components/nav-header/src/UserInfo.vue
@@ -2,20 +2,28 @@
 import { computed } from '@vue/reactivity'
 import { UserFilled } from '@element-plus/icons-vue'
 import { useStore } from '@/store'
+import { useRouter } from 'vue-router'
+import { localCache } from '@/utils'
 
 const store = useStore()
+const router = useRouter()
 const name = computed(() => store.state.login.userInfo.name)
+
+const handleSignOut = () => {
+  localCache.removeCache('token')
+  router.push('/login')
+}
 </script>
 
 <template>
   <div class="user-info">
-    <el-dropdown size="small">
+    <el-dropdown>
       <el-avatar :size="30">{{ name }}</el-avatar>
       <template #dropdown>
         <el-dropdown-menu>
-          <el-dropdown-item icon="User">用户信息</el-dropdown-item>
-          <el-dropdown-item icon="Setting">系统管理</el-dropdown-item>
-          <el-dropdown-item icon="Close" divided>退出登录</el-dropdown-item>
+          <el-dropdown-item icon="SwitchButton" @click="handleSignOut">
+            退出登录
+          </el-dropdown-item>
         </el-dropdown-menu>
       </template>
     </el-dropdown>
