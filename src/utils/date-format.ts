a81199933d98eefd44ed30c74b78cca5d53db62c
wangrui <wangruiself@163.com>
2022-06-23 01:48:52 +0800

    fix: 修改utc0时区的时间差

--- src/utils/date-format.ts
+++ src/utils/date-format.ts
@@ -12,7 +12,7 @@ export function formatUTCString(
   utcString: string,
   format: string = DATE_TIME_FORMAT
 ) {
-  return dayjs.utc(utcString).format(format)
+  return dayjs.utc(utcString).utcOffset(8).format(format)
 }
 
 // 转化时间戳格式
