5adb47ede71743c44995787ba049342b8ed00309
wangrui <wangruiself@163.com>
2022-06-26 22:34:26 +0800

    feat: add pro BASE_URL

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -4,7 +4,7 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = ''
+  BASE_URL = 'http://152.136.185.210:5000'
 } else {
   BASE_URL = ''
 }
