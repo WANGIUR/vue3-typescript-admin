3f71b02c98ae5f6e590a47ce6ffc42583ccbd3fd
wangrui <wangruiself@163.com>
2022-07-07 14:43:40 +0800

    feat: update view

--- src/views/main/analysis/overview/Overview.vue
+++ src/views/main/analysis/overview/Overview.vue
@@ -1,14 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import WrCard from '@/components/card'
+</script>
 
 <template>
   <div class="overview">
-    <div>
-      核心技术请见:
-      <a href="https://github.com/WANGIUR/vue3-typescript-admin" target="_blank"
-        >GitHub</a
-      >
-    </div>
-    <div>待完善</div>
+    <WrCard title="关于">
+      <div>
+        vue3-typescript-admin 是基于 Vue3、Vue Router、Vuex、Element
+        Plus、Echarts5、Webpack 的后台管理系统解决方案
+      </div>
+    </WrCard>
   </div>
 </template>
 
