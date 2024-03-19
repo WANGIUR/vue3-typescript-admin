99e997b8a369a381a588da276de7f5d021e7afe1
wangrui <wangruiself@163.com>
2024-03-19 12:07:52 +0800

    fix: 更新接口地址

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,8 @@ module.exports = defineConfig({
     devServer: {
       proxy: {
         '/api': {
-          target: 'http://152.136.185.210:5000',
+          // target: 'http://152.136.185.210:5000',
+          target: 'http://123.207.32.32:5000',
           pathRewrite: { '^/api': '' },
           changeOrigin: true
         }
